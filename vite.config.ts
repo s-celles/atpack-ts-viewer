12ed348f46f5f6bc2415c8ea9ed99764857c65cd
s-celles <s.celles@gmail.com>
2025-07-19 10:54:02 +0200

    feature: GH pages deployment - first try

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/atpack-ts-viewer/', // Nom du repository pour GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
